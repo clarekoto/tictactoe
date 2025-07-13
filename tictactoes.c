cc0dbd70309c1c3d1c4ec38cbf68545c184651bb
Clare Johnston <clarejohnston@Clares-MacBook-Air.local>
2025-07-13 18:20:16 +1000

    adds function valid_move() which checks if the users move was valid

--- tictactoes.c
+++ tictactoes.c
@@ -10,6 +10,8 @@ const int maxBufferSize = 80;
 
 typedef struct {
 	char grid[3][3];
+	int row;
+	int col;
 } Board;
 
 void init_board(Board* board) {
@@ -20,11 +22,10 @@ void init_board(Board* board) {
 	}
 }
 
-char getPlayer(void) {
+char get_player(void) {
         printf("Would you like to play as X or O ?\n");
         char player;
 	while(1) {
-
 		char buffer[maxBufferSize];
 		fgets(buffer, maxBufferSize, stdin);
 		player = toupper(buffer[0]);
@@ -40,9 +41,9 @@ char getPlayer(void) {
 }  
 
 void print_board(Board board) {
-	printf("  0 1 2\n");
+	printf("  1 2 3\n");
 	for (int i = 0; i < 3; i++) {
-		printf("%d|", i);
+		printf("%d|", i+1);
 		for (int j = 0; j < 3; j++) {
 			printf("%c|", board.grid[i][j]);
 		}
@@ -51,26 +52,31 @@ void print_board(Board board) {
 	printf("\n");
 }
 
-void valid_move(char* move, Board board) {
-	while(move[0]) {
-		printf("%c\n", move[0]);
-		move++;
-	}	
+bool valid_move(char* row, char* col, Board* board) {
+	int check_row = atoi(row);
+	int check_col = atoi(col);
+	if (!check_row || !check_col || check_row < 4 || check_col < 4) {
+		return false;
+	}
+	board->grid.row = check_row;
+	board->grid.col = check_col;
 }
 
 void get_move(Board* board, char player) {
-	printf("Please make a move in the format 1 2, where the former is the row and the latter the column\n");
- 	char buffer[maxBufferSize];
-	fgets(buffer, maxBufferSize, stdin);
-	char* move = strtok(buffer, " ");
-	valid_move(move, *board);	
+	printf("Where would you like to place a %c?\nRow: ", player);
+ 	char row[maxBufferSize];
+	fgets(row, maxBufferSize, stdin);
+	char col[maxBufferSize];
+	printf("Column: \n");
+	fgets(col, maxBufferSize, stdin);
+	valid_move(row, col, *board);	
 }
 
 int main(int argc, char* argv[]) {
 	Board board = {0};
 	init_board(&board);
 	printf("%s\n", welcome);
-	char player = getPlayer();
+	char player = get_player();
 	sleep(1);
 	print_board(board);
 	get_move(&board, player);
